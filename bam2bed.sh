85585d168db2fe1b9f47228c69942d4a93c33ea7
taraurselmann <taraurselmann@gmail.com>
2025-03-18 13:05:18 +0100

    Final assignment added

--- bam2bed.sh
+++ bam2bed.sh
@@ -1,16 +1,19 @@
+Input=$1
+Output_dir=$(realpath "$2")
+#Create output directory
+mkdir "$Output_dir"
+cd "$Output_dir"
 #Access conda 
+conda create -y --name bam2bed
+conda activate bam2bed
 source $(dirname $(dirname $(which mamba)))/etc/profile.d/conda.sh
-
-#Make the output directory 
-mkdir $output_dir
-cd $output_dir
-
-#Convert input from BAM to BED
-#load bedtools
+#Bedtools
 conda install -y bedtools
-
 #Using bedtools
-bamtobed -i $1 > $1.bed
-
-
-echo "Conversion complete"
+Output_file="$Output_dir/$(basename "$Input" | sed 's/\.bam$/.bed/')"
+bedtools bamtobed -i "$Input">$Output_file
+#Only chromosome 1
+Output_file_chr1="$Output_dir/$(basename "$Input" | sed 's/\.bam$/_chr1.bed/')"
+grep "^Chr1\s" $Output_file > $Output_file_chr1
+wc -l $Output_file_chr1>"bam2bed_number_of_rows.txt"
+echo "Completed by Tara Urselmann" 
