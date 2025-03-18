e7ff32672301284121d0c376e14778393e2b8b2a
taraurselmann <taraurselmann@gmail.com>
2025-03-18 14:01:50 +0100

    test

--- bam2bed.sh
+++ bam2bed.sh
@@ -10,11 +10,11 @@ source $(dirname $(dirname $(which mamba)))/etc/profile.d/conda.sh
 #Bedtools
 conda install -y bedtools
 #Using bedtools
-Output_file="$(basename "$Input" | sed 's/\.bam$/.bed/')"
+Output_file="$Output_dir/$(basename "$INPUT_BAM" | sed 's/\.bam$/.bed/')"
 bedtools bamtobed -i "$Input">$Output_file
 #Only chromosome 1
 Output_file_chr1="$(basename "$Input" | sed 's/\.bam$/_chr1.bed/')"
 grep "^Chr1\s" $Output_file > $Output_file_chr1
 wc -l $Output_file_chr1>"bam2bed_number_of_rows.txt"
-sed -i 's|\(GSM.*_chr1.bed\)|bam2bed_test_number3/\1|' bam2bed_test_number3/bam2bed_number_of_rows.txt
+#sed -i 's|\(GSM.*_chr1.bed\)|bam2bed_test_number3/\1|' bam2bed_test_number3/bam2bed_number_of_rows.txt
 echo "Completed by Tara Urselmann" 
