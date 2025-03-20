2237bccdf8ee69216ee1b6a3d06486992194df26
taraurselmann <taraurselmann@gmail.com>
2025-03-20 11:05:14 +0100

    final

--- bam2bed.sh
+++ bam2bed.sh
@@ -15,6 +15,7 @@ bedtools bamtobed -i "$Input">$Output_file
 #Only chromosome 1
 Output_file_chr1="$(basename "$Input" | sed 's/\.bam$/_chr1.bed/')"
 grep "^Chr1\s" $Output_file > $Output_file_chr1
-cp $Output_file_chr1 "$2/$(basename "$Input" | sed 's/\.bam$/_chr1.bed/')"
-wc -l "$2/$(basename "$Input" | sed 's/\.bam$/_chr1.bed/')">"bam2bed_number_of_rows.txt"
+Other_name="$2/$(basename "$Input" | sed 's/\.bam$/_chr1.bed/')"
+cp $Output_file_chr1 $Other_name
+wc -l $Other_name>"bam2bed_number_of_rows.txt"
 echo -e "Completed by Tara Urselmann" 
