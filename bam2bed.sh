3dec16f17a69476869fcbfb7b845db687f0ce57d
taraurselmann <taraurselmann@gmail.com>
2025-03-20 10:43:39 +0100

    final

--- bam2bed.sh
+++ bam2bed.sh
@@ -10,10 +10,10 @@ source $(dirname $(dirname $(which mamba)))/etc/profile.d/conda.sh
 #Bedtools
 conda install -y bedtools
 #Using bedtools
-Output_file="$Output_dir/$(basename "$Input" | sed 's/\.bam$/.bed/')"
+Output_file="$(basename "$Input" | sed 's/\.bam$/.bed/')"
 bedtools bamtobed -i "$Input">$Output_file
 #Only chromosome 1
-Output_file_chr1="$Output_dir/$(basename "$Input" | sed 's/\.bam$/_chr1.bed/')"
+Output_file_chr1="$(basename "$Input" | sed 's/\.bam$/_chr1.bed/')"
 grep "^Chr1\s" $Output_file > $Output_file_chr1
 wc -l $Output_file_chr1>"bam2bed_number_of_rows.txt"
 echo -e "Completed by Tara Urselmann" 
